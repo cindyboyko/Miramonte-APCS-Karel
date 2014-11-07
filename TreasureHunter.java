273b49c3d96e506a2c2c711aeeb914552e196643
Nathan Baum <nbaum510@gmail.com>
2014-11-07 14:10:28 -0800

    TreasureHunter

---
+++ TreasureHunter.java
@@ -0,0 +1,26 @@
+import kareltherobot.*;
+
+public class TreasureHunter
+{
+    // instance variables - replace the example below with your own
+    final int NORTH = 1, EAST = 2, SOUTH = 3, WEST = 4;
+
+    
+    public TreasureHunter()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
